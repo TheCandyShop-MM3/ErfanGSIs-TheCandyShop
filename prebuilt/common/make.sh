7f31fee7c6e69a7d3f4745c6b81ca034f02d6724
spydermahn1234 <75015413+spydermahn1234@users.noreply.github.com>
2020-12-17 15:16:03 +0000

    Update make.sh

--- prebuilt/common/make.sh
+++ prebuilt/common/make.sh
@@ -58,8 +58,8 @@ sed -i "/ro.actionable_compatible_property.enabled/d" $1/etc/prop.default
 sed -i "/ro.setupwizard.mode/d" $1/etc/prop.default
 sed -i "/ro.setupwizard.mode/d" $1/build.prop
 sed -i "/ro.setupwizard.mode/d" $1/product/build.prop
-echo "ro.setupwizard.mode=DISABLED" >> $1/etc/prop.default
-echo "ro.setupwizard.mode=DISABLED" >> $1/product/build.prop
+echo "ro.setupwizard.mode=OPTIONAL" >> $1/etc/prop.default
+echo "ro.setupwizard.mode=OPTIONAL" >> $1/product/build.prop
 # Some adding in build.prop
 sed -i "/sys.use_fifo_ui/d" $1/build.prop
 sed -i "/debug.sf.latch_unsignaled/d" $1/build.prop
